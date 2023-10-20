f24792e2db219b8fb1a5af7d2831afe427b48753
Eric-11 <svsarana@yahoo.com>
2023-10-20 09:34:03 -1000

    Update GarminApi.php
    Added code comments. Method getUserPermissions, dbg_api for tracking API calls

--- src/GarminApi.php
+++ src/GarminApi.php
@@ -1,5 +1,34 @@
 <?php
 
+/**
+ * This class serves as a part of a PHP application that interacts with the Garmin API, 
+ * enabling the retrieval of user data related to activities and fitness tracking. 
+ * It uses OAuth 1.0a for authentication and data access.
+ * 
+ *  It defines constants for the API base URLs (API_URL and USER_API_URL).
+ * 
+ * It implements several methods required for OAuth 1.0a:
+ * urlTemporaryCredentials: Returns the URL for retrieving temporary credentials (request tokens).
+ * urlAuthorization: Returns the URL where the user should be redirected to authorize the client.
+ * urlTokenCredentials: Returns the URL for retrieving token credentials (access tokens).
+ * getAuthorizationUrl: Generates the authorization URL based on the temporary credentials.
+ * getTokenCredentials: Retrieves token credentials using temporary credentials and a verifier.
+ * protocolHeader: Generates the OAuth protocol header for requests.
+ * baseProtocolParameters: Returns base protocol parameters for OAuth requests.
+ * 
+ * It defines a dbg_api method for making API requests with additional logging for debugging purposes.
+ *  It logs both the request and the response to a file.
+ * It provides methods for various API endpoints, including getting activity summaries, activity files,
+ *  manually updated activities, activity details, and more. These methods send HTTP requests to the
+ *  Garmin API and return the responses.
+ * It includes methods for backfilling different types of summary data such as activity summaries,
+ *  daily summaries, epoch summaries, and others.
+ * It includes methods for deleting user access tokens and getting user details.
+ * The class also provides methods to handle various OAuth-related functions like getting the user ID,
+ *  email, and screen name.
+ * 
+ */
+
 namespace Stoufa\GarminApi;
 
 use DateTime;
@@ -14,6 +43,45 @@ use League\OAuth1\Client\Credentials\TemporaryCredentials;
 use League\OAuth1\Client\Credentials\TokenCredentials;
 use League\OAuth1\Client\Server\User;
 
+/**
+ * The `GarminApi` class depends on several external classes and interfaces, and it utilizes them to implement OAuth 1.0a 
+ * authentication and interact with the Garmin API. Here's how it depends on each of the mentioned imports:
+ * 
+ * 1. `use League\OAuth1\Client\Server\Server`: The `GarminApi` class extends the `Server` class from the
+ * `League\OAuth1\Client\Server` namespace. This is a crucial dependency because the `Server` class is 
+ * the foundation for implementing OAuth 1.0a, and the `GarminApi` class builds upon its functionality 
+ * to perform the authentication flow.
+ * 
+ * 2. `use GuzzleHttp\Exception\BadResponseException`: This class is used to handle exceptions that occur
+ * when there's a bad response from the Garmin API. It allows the `GarminApi` class to catch and handle 
+ * errors during HTTP requests.
+ * 
+ * 3. `use GuzzleHttp\Exception\GuzzleException`: This exception class is also used to handle more general
+ * Guzzle HTTP client exceptions, providing additional error-handling capabilities.
+ * 
+ * 4. `use InvalidArgumentException`: This exception class is used to handle cases where invalid arguments are
+ * provided to various methods. It helps ensure that the inputs to the class's methods are valid.
+ * 
+ * 5. `use League\OAuth1\Client\Credentials\CredentialsException`: This exception class is used for handling
+ * exceptions related to OAuth credentials. If there are issues with credentials, this class allows the 
+ * `GarminApi` class to handle them gracefully.
+ * 
+ * 6. `use League\OAuth1\Client\Credentials\CredentialsInterface`: The `CredentialsInterface` is an interface
+ * that defines the contract for credential objects. The `GarminApi` class uses this interface to work with 
+ * various types of credentials, such as temporary credentials and token credentials.
+ * 
+ * 7. `use League\OAuth1\Client\Credentials\TemporaryCredentials`: This class represents temporary credentials
+ * (request tokens) used during the OAuth 1.0a authentication process. The `GarminApi` class uses it when 
+ * requesting temporary credentials and exchanging them for token credentials.
+ * 
+ * 8. `use League\OAuth1\Client\Credentials\TokenCredentials`: This class represents token credentials (access
+ * tokens) used to access protected resources. The `GarminApi` class uses it when sending authenticated 
+ * requests to the Garmin API.
+ * 
+ * 9. `use League\OAuth1\Client\Server\User`: The `User` class is used to represent user data retrieved from the
+ * Garmin API. The `GarminApi` class includes methods for obtaining user details, and it uses the `User` class to 
+ * structure and return user information.
+ */
 class GarminApi extends Server {
 
     /**
@@ -197,7 +265,12 @@ class GarminApi extends Server {
         }
         if ($log) {
             fwrite($fp, "RESPONSE: " . $response->getStatusCode() . " : " . $response->getReasonPhrase());
-            fwrite($fp, "\nBODY:\n" . $response->getBody());
+            if (preg_match('~[^\x20-\x7E\t\r\n]~', $response->getBody()) > 0) {
+                fwrite($fp, "BODY:\n[contains binary data]");
+                fwrite($fp, $response->getBody());
+            } else {
+                fwrite($fp, "\nBODY:\n" . $response->getBody());
+            }
             fclose($fp);
         }
         return $response->getBody();
@@ -217,7 +290,7 @@ class GarminApi extends Server {
         $query = 'activities?' . $query;
         $headers = $this->getHeaders($tokenCredentials, 'GET', self::USER_API_URL . $query);
 
-        $response = $this->dbg_api($client, $query, $headers);
+        $response = $this->dbg_api($client, '', $headers);
 
         return $response;
     }
@@ -368,7 +441,7 @@ class GarminApi extends Server {
     public function backfillSleepSummary(TokenCredentials $tokenCredentials, array $params): void {
         $this->backfill($tokenCredentials, 'sleep', $params);
     }
-    
+
     /**
      * Send request for Heart Rate Variability Data
      * 
@@ -379,7 +452,7 @@ class GarminApi extends Server {
     public function backfillHRV(TokenCredentials $tokenCredentials, array $params): void {
         $this->backfill($tokenCredentials, "hrv", $params);
     }
-    
+
     /**
      * send request to back fill body composition summary
      *
@@ -516,7 +589,7 @@ class GarminApi extends Server {
     }
 
     /**
-     * Left for compatibilty
+     * Left for compatiblity
      *
      * @param mixed $data
      * @param TokenCredentials $tokenCredentials
@@ -526,4 +599,32 @@ class GarminApi extends Server {
         return '';
     }
 
+    /**
+     * Request permissions allowed by user
+     * @param type $tokenCredentials
+     * @return string
+     *      Typical response
+     *   {"permissions": "[ACTIVITY_EXPORT, WORKOUT_IMPORT, HEALTH_EXPORT, COURSE_IMPORT, MCT_EXPORT]"}
+     */
+    public function getUserPermissions($tokenCredentials): string {
+        $query = 'user/permissions';
+        $client = $this->createHttpClient();
+        $headers = $this->getHeaders($tokenCredentials, 'GET', self::USER_API_URL . $query);
+
+        try {
+            $response = $client->get(self::USER_API_URL . $query, [
+                'headers' => $headers,
+            ]);
+        } catch (BadResponseException $e) {
+            $response = $e->getResponse();
+            $body = $response->getBody();
+            $statusCode = $response->getStatusCode();
+            throw new \Exception(
+                            "getUserPermissions Received error [$body] with status code [$statusCode] when retrieving manually activity summary."
+            );
+        }
+        $this->dbg_api($client, $query, $headers);
+
+        return $response->getBody()->getContents();
+    }
 }
